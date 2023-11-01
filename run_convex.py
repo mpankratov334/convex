6b960ad757ce4d0693478cb91682ed973d36e95f
mpankratov334 <101637853+mpankratov334@users.noreply.github.com>
2023-11-01 19:15:19 +0300

    Update run_convex.py

--- run_convex.py
+++ run_convex.py
@@ -6,7 +6,8 @@ f = Void()
 try:
     while True:
         f = f.add(R2Point())
-        print(f"S = {f.area()}, P = {f.perimeter()}, Sum Squares = {f.sum_squares()}")
+        print(f"S = {f.area()}, P = {f.perimeter()}, 
+                Sum Squares = {f.sum_squares()}")
         print()
 except(EOFError, KeyboardInterrupt):
     print("\nStop")
